a573b2b541a6698a5ab01f196a55fa2857e7e3b5
balazs <balazs.sipocz@me.com>
2012-05-23 13:53:54 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
+  before_save :create_remember_token
   before_save { self.email.downcase! }
 # before_save { |user| user.email = email.downcase}
 
@@ -23,4 +24,10 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+  	def create_remember_token
+  		self.remember_token = SecureRandom.urlsafe_base64
+  	end
 end
