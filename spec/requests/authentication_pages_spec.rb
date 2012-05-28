f40d58008af82591db0b1afde294514230cdb440
Balazs <balazs.sipocz@me.com>
2012-05-28 22:26:15 +0200

    Finish user destroy action

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -84,6 +84,18 @@ describe "Authentication" do
 			end
 		end
 
+		describe "ad non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { valid_signin non_admin }
+
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		end
+
 		describe "as wrong user" do
 			let(:user) { FactoryGirl.create(:user) }
 			let(:wrong_user) {FactoryGirl.create(:user, email: "wrong@example.com") }
